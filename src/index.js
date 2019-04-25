94ff21a3615ffd4a7ab08db423726249f09ac63b
Jorge L <jorge.alfmur@gmail.com>
2019-04-25 11:49:01 -0500

    add request obj into context

--- src/index.js
+++ src/index.js
@@ -25,7 +25,12 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: { prisma },
+    context: request => {
+      return {
+        ...request,
+        prisma,
+      }
+    },
 })
 
 server.start(() => console.log(`server is running on http://localhost:4000 🚀`))
