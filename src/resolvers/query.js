cd0f6daadb73640ed86f00ea28477a2013aeabf9
Jorge L <jorge.alfmur@gmail.com>
2019-04-27 01:11:23 -0500

    sorting, filtering and pagination

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,4 +1,20 @@
-const feed = (_, args, context, info) => context.prisma.links()
+const feed = async (_, args, context, info) => {
+    const where = args.filter ? {
+        OR: [
+            { description_contains: args.filter},
+            { url_contains: args.filter},
+        ],
+    } : {}
+    const links = await context.prisma.links({
+        where,
+        skip: args.skip,
+        first: args.first,
+        orderBy: args.orderBy,
+    })
+
+    return links
+}
+
 const link = (_, args, context) => context.prisma.link({ id: args.id })
 
 module.exports = {
